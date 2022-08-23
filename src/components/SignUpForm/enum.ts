edeb1642272d47c8123d811c58f6f5c6437ec349
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-08-23 21:20:18 +0100

    FE(DEV): Add functional sign up form

---
+++ src/components/SignUpForm/enum.ts
@@ -0,0 +1,7 @@
+export enum FORM_KEYS {
+    USERNAME = 'username',
+    EMAIL = 'email',
+    PASSWORD = 'password',
+    PASSWORD_CONFIRMED = 'passwordConfirmed',
+    TERMS_CONFIRMED = 'termsConfirmed',
+}
--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,21 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FORM_KEYS } from './enum';
 
 const SignUpForm = () => {
+    const [formData, setFormData] = useState({
+        [FORM_KEYS.USERNAME]: '',
+        [FORM_KEYS.EMAIL]: '',
+        [FORM_KEYS.PASSWORD]: '',
+        [FORM_KEYS.PASSWORD_CONFIRMED]: '',
+        [FORM_KEYS.TERMS_CONFIRMED]: false,
+    });
+    const onChange = (e: { target: HTMLInputElement }, key: String) => {
+        setFormData(prevState => {
+            switch (key) {
+                case FORM_KEYS.USERNAME:
+                    return { ...prevState, [FORM_KEYS.USERNAME]: e.target.value };
+                case FORM_KEYS.EMAIL:
+                    return { ...prevState, [FORM_KEYS.EMAIL]: e.target.value };
+                case FORM_KEYS.PASSWORD:
+                    return { ...prevState, [FORM_KEYS.PASSWORD]: e.target.value };
+                case FORM_KEYS.PASSWORD_CONFIRMED:
+                    return { ...prevState, [FORM_KEYS.PASSWORD_CONFIRMED]: e.target.value };
+                case FORM_KEYS.TERMS_CONFIRMED:
+                    return { ...prevState, [FORM_KEYS.TERMS_CONFIRMED]: e.target.checked };
+                default:
+                    return prevState;
+            }
+        });
+    };
+
     return (
-        <form>
+        <form data-testid='form'>
             <h1>Sign Up</h1>
             <h2>Create an account</h2>
-            <input placeholder='Username' />
-            <input placeholder='Email' />
-            <input placeholder='Password' />
-            <input placeholder='Confirm Password' />
-            <input placeholder='Confirm Password' />
+            <input
+                onChange={e => onChange(e, FORM_KEYS.USERNAME)}
+                type='text'
+                placeholder='Username'
+                value={formData[FORM_KEYS.USERNAME]}
+            />
+            <input
+                onChange={e => onChange(e, FORM_KEYS.EMAIL)}
+                type='text'
+                placeholder='Email'
+                value={formData.email}
+            />
+            <input
+                onChange={e => onChange(e, FORM_KEYS.PASSWORD)}
+                type='password'
+                placeholder='Password'
+                value={formData.password}
+            />
+            <input
+                onChange={e => onChange(e, FORM_KEYS.PASSWORD_CONFIRMED)}
+                type='password'
+                placeholder='Confirm Password'
+                value={formData[FORM_KEYS.PASSWORD_CONFIRMED]}
+            />
             <label>
                 I have read and agree to the terms and conditions.
-                <input type='checkbox' />
+                <input
+                    onChange={e => onChange(e, FORM_KEYS.TERMS_CONFIRMED)}
+                    checked={formData.termsConfirmed}
+                    type='checkbox'
+                />
             </label>
             <p>Error Message</p>
-            <a href='http://example.com'>Already have an account? Log in here.</a>
+            <Link to={'/login'}>Already have an account? Log in here.</Link>
             <button>Sign Up</button>
         </form>
     );
