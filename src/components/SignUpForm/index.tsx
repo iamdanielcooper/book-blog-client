538df767fff47b88d272ab0e01c35e69b3546d22
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-09-10 13:33:50 +0100

    FE(DEV): refactor sign up flow

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -2,20 +2,15 @@ import axios, { AxiosResponse } from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FORM_STATUS } from './enum';
-const endpoint = 'http://localhost:4000/users/register';
 
-const SignUpForm = () => {
+const SignUpForm = ({ formStatus, setFormStatus, setFormData }: any) => {
     const [username, setUsername] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [confirmedPassword, setConfirmedPassword] = useState<string>('');
     const [termsAgreed, setTermsAgreed] = useState<boolean>(false);
 
-    const [formStatus, setFormStatus] = useState<string>(
-        FORM_STATUS.AWAITING_USER_INPUT
-    );
     const [errorMessage, setErrorMessage] = useState<string>('');
-    const [serverResponse, setServerResponse] = useState<AxiosResponse>();
 
     const formIsComplete = () => {
         return (
@@ -28,41 +23,15 @@ const SignUpForm = () => {
         );
     };
 
-    useEffect(() => {
-        const sendUser = async () => {
-            try {
-                const response: AxiosResponse = await axios.post(endpoint, {
-                    username: username,
-                    password: password,
-                    email: email,
-                    isAdmin: false,
-                });
-                console.log(response);
-
-                setServerResponse(response);
-            } catch (error) {
-                setErrorMessage('');
-            }
-        };
-        if (formStatus === FORM_STATUS.SENDING) {
-            sendUser();
-        }
-    }, [formStatus]);
-
-    useEffect(() => {
-        switch (serverResponse?.status) {
-            case 201:
-                setFormStatus(FORM_STATUS.SUCCESS);
-                break;
-            default:
-                setFormStatus(FORM_STATUS.AWAITING_USER_INPUT);
-                break;
-        }
-    }, [serverResponse, errorMessage]);
-
     const handleSubmit = (e: any) => {
         e.preventDefault();
         if (formIsComplete()) {
+            setFormData({
+                username: username,
+                password: password,
+                email: email,
+                isAdmin: false,
+            });
             setFormStatus(FORM_STATUS.SENDING);
         } else {
             setFormStatus(FORM_STATUS.AWAITING_USER_INPUT);
@@ -70,62 +39,47 @@ const SignUpForm = () => {
         }
     };
 
-    const renderComponent = () => {
-        switch (formStatus) {
-            case FORM_STATUS.AWAITING_USER_INPUT:
-                return (
-                    <form data-testid='form' onSubmit={handleSubmit}>
-                        <h1>Sign Up</h1>
-                        <h2>Create an account</h2>
-                        <input
-                            onChange={e => setUsername(e.target.value)}
-                            type='text'
-                            placeholder='Username'
-                            value={username}
-                        />
-                        <input
-                            onChange={e => setEmail(e.target.value)}
-                            type='text'
-                            placeholder='Email'
-                            value={email}
-                        />
-                        <input
-                            onChange={e => setPassword(e.target.value)}
-                            type='password'
-                            placeholder='Password'
-                            value={password}
-                        />
-                        <input
-                            onChange={e => setConfirmedPassword(e.target.value)}
-                            type='password'
-                            placeholder='Confirm Password'
-                            value={confirmedPassword}
-                        />
-                        <label>
-                            I have read and agree to the terms and conditions.
-                            <input
-                                onChange={e => setTermsAgreed(e.target.checked)}
-                                checked={termsAgreed}
-                                type='checkbox'
-                            />
-                        </label>
-                        <p>{errorMessage}</p>
-                        <Link to={'/login'}>
-                            Already have an account? Log in here.
-                        </Link>
-                        <button>Sign Up</button>
-                    </form>
-                );
-            case FORM_STATUS.SENDING:
-                return <h1>Sending</h1>;
-            case FORM_STATUS.SUCCESS:
-                return <h1>Sent ✅</h1>;
-            default:
-                return <h1>Loading</h1>;
-        }
-    };
-
-    return <>{renderComponent()}</>;
+    return (
+        <form data-testid='form' onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+            <h2>Create an account</h2>
+            <input
+                onChange={e => setUsername(e.target.value)}
+                type='text'
+                placeholder='Username'
+                value={username}
+            />
+            <input
+                onChange={e => setEmail(e.target.value)}
+                type='text'
+                placeholder='Email'
+                value={email}
+            />
+            <input
+                onChange={e => setPassword(e.target.value)}
+                type='password'
+                placeholder='Password'
+                value={password}
+            />
+            <input
+                onChange={e => setConfirmedPassword(e.target.value)}
+                type='password'
+                placeholder='Confirm Password'
+                value={confirmedPassword}
+            />
+            <label>
+                I have read and agree to the terms and conditions.
+                <input
+                    onChange={e => setTermsAgreed(e.target.checked)}
+                    checked={termsAgreed}
+                    type='checkbox'
+                />
+            </label>
+            <p>{errorMessage}</p>
+            <Link to={'/login'}>Already have an account? Log in here.</Link>
+            <button>Sign Up</button>
+        </form>
+    );
 };
 
 export default SignUpForm;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,64 @@
-import React, { useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
 import { SignUpForm } from '../../components';
+import { FORM_STATUS } from '../../components/SignUpForm/enum';
+const endpoint = 'http://localhost:4000/users/register';
 
 const SignUp = () => {
-    return <SignUpForm />;
+    const [formStatus, setFormStatus] = useState<string>(
+        FORM_STATUS.AWAITING_USER_INPUT
+    );
+    const [formData, setFormData] = useState({});
+    const [serverResponse, setServerResponse] = useState<AxiosResponse>();
+
+    useEffect(() => {
+        const sendUser = async () => {
+            try {
+                const response: AxiosResponse = await axios.post(
+                    endpoint,
+                    formData
+                );
+                console.log(response);
+
+                setServerResponse(response);
+            } catch (error) {}
+        };
+        if (formStatus === FORM_STATUS.SENDING) {
+            sendUser();
+        }
+    }, [formStatus]);
+
+    useEffect(() => {
+        switch (serverResponse?.status) {
+            case 201:
+                setFormStatus(FORM_STATUS.SUCCESS);
+                break;
+            default:
+                setFormStatus(FORM_STATUS.AWAITING_USER_INPUT);
+                break;
+        }
+    }, [serverResponse]);
+
+    const renderComponent = () => {
+        switch (formStatus) {
+            case FORM_STATUS.AWAITING_USER_INPUT:
+                return (
+                    <SignUpForm
+                        formStatus={formStatus}
+                        setFormStatus={setFormStatus}
+                        setFormData={setFormData}
+                    />
+                );
+            case FORM_STATUS.SENDING:
+                return <h1>Sending</h1>;
+            case FORM_STATUS.SUCCESS:
+                return <h1>Sent ✅</h1>;
+            default:
+                return <h1>Loading</h1>;
+        }
+    };
+
+    return <>{renderComponent()}</>;
 };
 
 export default SignUp;
